50d19210ba0460854bf5300c57a50d83f5a5f54e
Jessica Ross <deathweasel@deathweasel.net>
2012-07-30 23:23:51 +0000

    Layout. Added some authentication things. Altered some templates, most notably the base template.

---
+++ deathweasel/artwork/forms.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vim: fileencoding=utf-8 tabstop=4 expandtab shiftwidth=4
+from models import ArtworkModel, CommentModel
+from django.forms import ModelForm
+
+
+class ArtworkForm(ModelForm):
+    class Meta:
+        model=ArtworkModel
+        exclude = ('upload_date','artist')
+
+class CommentForm(ModelForm):
+    class Meta:
+        model=CommentModel
+        exclude = ('date',)
