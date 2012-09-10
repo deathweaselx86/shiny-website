c1d6c2328428f375ef42e81fac982a7bca417e30
Jessica Ross <deathweasel@deathweasel.net>
2012-09-10 04:19:05 +0000

    Probably should have written the unit tests before I started the site...

--- deathweasel/artwork/tests.py
+++ deathweasel/artwork/tests.py
@@ -1,16 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8; mode: python; py-indent-offset: 4; indent-tabs-mode: nil -*-
+# vim: fileencoding=utf-8 tabstop=4 expandtab shiftwidth=4
+
 """
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+This module contains tests for the artwork module.
+>_> Should have wrote all of these before I did anything...
+Mea culpa.
 
-Replace this with more appropriate tests for your application.
 """
 
-from django.test import TestCase
+from django.test import LiveServerTestCase
+from selenium import webdriver
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class ArtworkTest(LiveServerTestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+        self.browser.implicitly_wait(3)
+    
+    def tearDown(self):
+        self.browser.quit()
+    
+    # Can we get to where we should be able to from
+    # the front page?
+    def can_view_front_page(self):
+        self.browser.get(self.live_server_url)
+        title = self.browser.find_element_by_tag('title')
+        self.assertIn("deathweasel.net",title)
+    
+    def can_access_artwork_page(self):
+        self.browser.get(self.live_server_url)
+        artwork = self.browser.find_element_by_link_text("Artwork")
+
