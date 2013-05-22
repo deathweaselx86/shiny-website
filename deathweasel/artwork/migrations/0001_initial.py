f64b9974e582be65155e9e3641a8cd8b248a2d04
Jessica Ross <deathweasel@deathweasel.net>
2013-05-22 01:05:46 +0000

    South migrations.

---
+++ deathweasel/artwork/migrations/0001_initial.py
@@ -0,0 +1,131 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'ArtworkModel'
+        db.create_table('artwork_artworkmodel', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('medium', self.gf('django.db.models.fields.CharField')(default='graphite', max_length=200)),
+            ('upload_date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('artist', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('desc', self.gf('django.db.models.fields.TextField')(max_length=500)),
+        ))
+        db.send_create_signal('artwork', ['ArtworkModel'])
+
+        # Adding M2M table for field keywords on 'ArtworkModel'
+        db.create_table('artwork_artworkmodel_keywords', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('artworkmodel', models.ForeignKey(orm['artwork.artworkmodel'], null=False)),
+            ('keywordmodel', models.ForeignKey(orm['artwork.keywordmodel'], null=False))
+        ))
+        db.create_unique('artwork_artworkmodel_keywords', ['artworkmodel_id', 'keywordmodel_id'])
+
+        # Adding model 'CommentModel'
+        db.create_table('artwork_commentmodel', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('author', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=250)),
+            ('body', self.gf('django.db.models.fields.TextField')()),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('parent', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['artwork.CommentModel'], unique=True, null=True)),
+            ('artwork', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['artwork.ArtworkModel'])),
+        ))
+        db.send_create_signal('artwork', ['CommentModel'])
+
+        # Adding model 'KeywordModel'
+        db.create_table('artwork_keywordmodel', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('keyword', self.gf('django.db.models.fields.CharField')(max_length=200)),
+        ))
+        db.send_create_signal('artwork', ['KeywordModel'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'ArtworkModel'
+        db.delete_table('artwork_artworkmodel')
+
+        # Removing M2M table for field keywords on 'ArtworkModel'
+        db.delete_table('artwork_artworkmodel_keywords')
+
+        # Deleting model 'CommentModel'
+        db.delete_table('artwork_commentmodel')
+
+        # Deleting model 'KeywordModel'
+        db.delete_table('artwork_keywordmodel')
+
+
+    models = {
+        'artwork.artworkmodel': {
+            'Meta': {'ordering': "['title']", 'object_name': 'ArtworkModel'},
+            'artist': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'desc': ('django.db.models.fields.TextField', [], {'max_length': '500'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'keywords': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['artwork.KeywordModel']", 'symmetrical': 'False'}),
+            'medium': ('django.db.models.fields.CharField', [], {'default': "'graphite'", 'max_length': '200'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'upload_date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'})
+        },
+        'artwork.commentmodel': {
+            'Meta': {'ordering': "['-date']", 'object_name': 'CommentModel'},
+            'artwork': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['artwork.ArtworkModel']"}),
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['artwork.CommentModel']", 'unique': 'True', 'null': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        'artwork.keywordmodel': {
+            'Meta': {'object_name': 'KeywordModel'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'keyword': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 5, 14, 21, 16, 30, 171317, tzinfo=<UTC>)'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 5, 14, 21, 16, 30, 171186, tzinfo=<UTC>)'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        }
+    }
+
+    complete_apps = ['artwork']
---
+++ deathweasel/posts/migrations/0001_initial.py
@@ -0,0 +1,121 @@
+# encoding: utf-8
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        
+        # Adding model 'PostModel'
+        db.create_table('posts_postmodel', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=250)),
+            ('user', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.User'])),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+            ('body', self.gf('django.db.models.fields.TextField')()),
+            ('allow_comments', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('viewable', self.gf('django.db.models.fields.BooleanField')(default=False)),
+        ))
+        db.send_create_signal('posts', ['PostModel'])
+
+        # Adding M2M table for field category on 'PostModel'
+        db.create_table('posts_postmodel_category', (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('postmodel', models.ForeignKey(orm['posts.postmodel'], null=False)),
+            ('keywordmodel', models.ForeignKey(orm['artwork.keywordmodel'], null=False))
+        ))
+        db.create_unique('posts_postmodel_category', ['postmodel_id', 'keywordmodel_id'])
+
+        # Adding model 'CommentModel'
+        db.create_table('posts_commentmodel', (
+            ('id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('author', self.gf('django.db.models.fields.CharField')(max_length=200)),
+            ('title', self.gf('django.db.models.fields.CharField')(max_length=250)),
+            ('body', self.gf('django.db.models.fields.TextField')()),
+            ('date', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('parent', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['posts.CommentModel'], unique=True, null=True)),
+            ('post', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['posts.PostModel'])),
+        ))
+        db.send_create_signal('posts', ['CommentModel'])
+
+
+    def backwards(self, orm):
+        
+        # Deleting model 'PostModel'
+        db.delete_table('posts_postmodel')
+
+        # Removing M2M table for field category on 'PostModel'
+        db.delete_table('posts_postmodel_category')
+
+        # Deleting model 'CommentModel'
+        db.delete_table('posts_commentmodel')
+
+
+    models = {
+        'artwork.keywordmodel': {
+            'Meta': {'object_name': 'KeywordModel'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'keyword': ('django.db.models.fields.CharField', [], {'max_length': '200'})
+        },
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 5, 14, 21, 16, 39, 858480, tzinfo=<UTC>)'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime(2013, 5, 14, 21, 16, 39, 858346, tzinfo=<UTC>)'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'posts.commentmodel': {
+            'Meta': {'ordering': "['-date']", 'object_name': 'CommentModel'},
+            'author': ('django.db.models.fields.CharField', [], {'max_length': '200'}),
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent': ('django.db.models.fields.related.OneToOneField', [], {'to': "orm['posts.CommentModel']", 'unique': 'True', 'null': 'True'}),
+            'post': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['posts.PostModel']"}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'})
+        },
+        'posts.postmodel': {
+            'Meta': {'ordering': "['-date']", 'object_name': 'PostModel'},
+            'allow_comments': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'body': ('django.db.models.fields.TextField', [], {}),
+            'category': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['artwork.KeywordModel']", 'symmetrical': 'False'}),
+            'date': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'title': ('django.db.models.fields.CharField', [], {'max_length': '250'}),
+            'user': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']"}),
+            'viewable': ('django.db.models.fields.BooleanField', [], {'default': 'False'})
+        }
+    }
+
+    complete_apps = ['posts']
